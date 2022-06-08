31c10f4015a3157b559d800cb2fb1adfd102bb51
SozinovaPS <apolinas225@mail.ru>
2022-06-08 20:39:59 +0300

    Something was written :>

--- main.c
+++ main.c
@@ -6,6 +6,16 @@
 #include <string.h>
 
 int main(){
-	printf ("HAMPTER!");
+	char* input_Path = "my.png";
+	int iw, ih, n;
+	unsigned char* idata = stbi_load(input_Path, &iw, &ih, &n, 0);
+	if (idata == NULL){
+		printf ("ERROR: can't read file! %s\n", input_Path);
+		return -1;
+	}
+	int i, j, k = 0;
+	unsigned char* odata = malloc(ih * iw * n * sizeof(unsigned char));
+	unsigned char* newImage = malloc(ih * iw * sizeof(unsigned char));
+	
 	return 0;
 }
