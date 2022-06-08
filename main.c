5d764c4440f113d9c7459535058e8cbe6acdbdd5
SozinovaPS <apolinas225@mail.ru>
2022-06-08 18:06:34 +0300

    MUHAHAHAHA XD

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include "stb_image.h"
+#include <stb_image_write.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
 int main(){
 	printf ("HAMPTER!");
 	return 0;
