bee88e4312c9ff486ac245201326afaa91488026
SozinovaPS <apolinas225@mail.ru>
2022-07-03 16:38:56 +0300

    You need to stop

--- main.c
+++ main.c
@@ -9,8 +9,8 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-#define BLACK 94
-#define WHITE 166
+#define BLACK 87
+#define WHITE 170
 
 unsigned char* color_to_grey(unsigned char* Image, int w, int h, int n){
 	int i, k = 0;
