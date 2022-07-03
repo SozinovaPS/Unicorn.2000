87354e2b2893695be3e0d519000cfe684f08e427
SozinovaPS <apolinas225@mail.ru>
2022-07-03 16:53:58 +0300

    Final!

--- main.c
+++ main.c
@@ -71,7 +71,7 @@ unsigned char* coloration(unsigned char* Img, int w, int h, int n, int size, int
 }
 
 int main(){
-	char* inputPath = "tooth.jpg";
+	char* inputPath = "Arrow_head.png";
 	int w, h, n;
 	unsigned char* data = stbi_load(inputPath, &w, &h, &n, 0);
 	if (data == NULL){
@@ -104,7 +104,7 @@ int main(){
 
 	My_img = coloration(odata, w, h, n, size, graph);
 
-	char* outputPath = "tooth_out.jpg";
+	char* outputPath = "arrow_head__out.jpg";
 	stbi_write_png(outputPath, w, h, n, My_img, 0);
 
 	printf ("^-^");
