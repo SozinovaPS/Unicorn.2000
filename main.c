a96c546abfc6ffad9a0854fb0548cb75731a8353
SozinovaPS <apolinas225@mail.ru>
2022-07-03 13:59:44 +0300

    Make some contrast

--- main.c
+++ main.c
@@ -8,6 +8,9 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#define BLACK 100
+#define WHITE 160
+
 unsigned char* color_to_grey(unsigned char* Image, int w, int h, int n){
 	int i, k = 0;
 	int size;
@@ -24,8 +27,20 @@ unsigned char* color_to_grey(unsigned char* Image, int w, int h, int n){
 	return gray_img;
 }
 
+unsigned char*  contrast( unsigned char* Image, int w, int h) {
+	int i, j;    	
+	for (i = 2; i < h - 1; i++)
+        	for (j = 2; j < w - 1; j++) {
+            		if (Image[w * i + j] < BLACK)
+				Image[w * i + j] = 0;
+            		if (Image[w * i + j] > WHITE)
+				Image[w * i + j] = 255;
+        	}
+	return Image;
+}
+
 int main(){
-	char* inputPath = "Rider-Karl-Bryullov-1.png";
+	char* inputPath = "hampster.png";
 	int w, h, n;
 	unsigned char* data = stbi_load(inputPath, &w, &h, &n, 0);
 	if (data == NULL){
@@ -40,7 +55,10 @@ int main(){
 		return 1;
         }
 	My_img = color_to_grey(data, w, h, n);
-	char* outputPath = "prekol.png";
+	
+	My_img = contrast(My_img, w, h);
+
+	char* outputPath = "final.png";
 	stbi_write_png(outputPath, w, h, 1, My_img, 0);
 
 	printf ("^-^");
